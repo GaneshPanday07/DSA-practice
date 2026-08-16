f0d22a42600e8e390494ac4baa9094add9714e75
unknown <ganeshp782791@gmail.com>
2026-08-16 18:21:36 +0530

    String

---
+++ String/name.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class name {
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter name : ");
+    String n = sc.nextLine();
+
+    System.out.print(n);
+}
+}
